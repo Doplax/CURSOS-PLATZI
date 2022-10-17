0df0634730f551641755f8e67c558baf19c23d15
Doplax <doplax@gmail.com>
2022-10-17 15:13:36 +0200

    En mitad de la clase de clousures

--- closures_scopes/ej5_strict_mode.js
+++ closures_scopes/ej5_strict_mode.js
@@ -1,2 +1,15 @@
-pi = 3.1416;
-console.log(pi);
+// "use strict"
+
+// console.log(pi);
+// 
+let pi;
+function myFunction() {
+    'use strict'
+    //  var pi;
+    return pi = 3.1416;
+ 
+    
+}
+ 
+
+console.log(myFunction());
