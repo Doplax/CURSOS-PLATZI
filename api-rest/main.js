b54b3f6e33871646008ab392509b3efdfb3ba2d7
Doplax <doplax@gmail.com>
2023-03-21 11:09:33 +0100

    La parte de la logica ya está acabada, ahora solo falta darle un poquito de css, y al portafolio <3

--- api-rest/main.js
+++ api-rest/main.js
@@ -1,25 +1,46 @@
-const API_URL_FAVOURITE = 'https://api.thecatapi.com/v1/favourites?limit=3&api_key=live_SZuu0JOx6kb9uvMjP1hLQrmAFFdUJXSaIkTL8mBaxzFvT6RZ7dj1uExeF6YUeIL6';
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_SZuu0JOx6kb9uvMjP1hLQrmAFFdUJXSaIkTL8mBaxzFvT6RZ7dj1uExeF6YUeIL6';
+let api = "live_SZuu0JOx6kb9uvMjP1hLQrmAFFdUJXSaIkTL8mBaxzFvT6RZ7dj1uExeF6YUeIL6"
+const MAX_ELEMENTS = "5"
+const API_URL_FAVOURITE = `https://api.thecatapi.com/v1/favourites?limit=${MAX_ELEMENTS}&api_key=live_SZuu0JOx6kb9uvMjP1hLQrmAFFdUJXSaIkTL8mBaxzFvT6RZ7dj1uExeF6YUeIL6`;
+const API_URL_RANDOM    = `https://api.thecatapi.com/v1/images/search?limit=${MAX_ELEMENTS}&api_key=live_SZuu0JOx6kb9uvMjP1hLQrmAFFdUJXSaIkTL8mBaxzFvT6RZ7dj1uExeF6YUeIL6`;
+const API_URL_FAVOTITES_DELETE = (id) =>  `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_SZuu0JOx6kb9uvMjP1hLQrmAFFdUJXSaIkTL8mBaxzFvT6RZ7dj1uExeF6YUeIL6`;
 
-const spanError = document.getElementById("error");
+
+const SPAN_ERROR = document.getElementById("error");
+
 
 async function loadRandomMichis(API_URL) {
-  const res = await fetch(API_URL);
-  const data = await res.json();
-  console.log("Random");
-  console.log(data)
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    console.log("Random");
+    console.log(data)
+    const randomMichis = document.getElementById("randomMichis");
+    randomMichis.innerHTML = "";
 
-  //En caso de no recibir un http 200 mostraremos el siguiente error:  
-  if (res.status !== "200")
-  {
-    spanError.innerHTML = "Hubo un error " + res.status
-  } else {
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
+    //En caso de no recibir un http 200 mostraremos el siguiente error: (recuerda compararlo con un número, no un string)
+    if (res.status !== 200) 
+    {
+      SPAN_ERROR.innerText = "El error es: " + res.status
+    } else {
     
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-  }
+      
+    
+      data.forEach(element => {
+        console.log();
+  
+        let article = document.createElement("article")
+        
+        let img = document.createElement("img")
+        img.src = element.url
+        article.appendChild(img)
+        
+        let btn = document.createElement("button")
+        btn.innerText = "Guardar"
+        btn.addEventListener("click", () => saveFavouriteMichis(element))
+        article.appendChild(btn)
+  
+        randomMichis.appendChild(article)
+      });
+    }
 }
 
 async function loadFavoritesMichis(API_URL) {
@@ -28,37 +49,79 @@ async function loadFavoritesMichis(API_URL) {
   console.log("Favoritos");
   console.log(data);
 
-  if (res.status !== "200")
+  const favoritesMichis = document.getElementById("favoritesMichis");
+  favoritesMichis.innerHTML = "";
+
+
+  if (res.status !== 200)
   {
-    spanError.innerHTML = "Hubo un error " + res.status;
+    SPAN_ERROR.innerText = "El error es: " + res.status
+  } else {
+    
+    
+    data.forEach(element => {
+      console.log();
+
+      let article = document.createElement("article")
+      
+      let img = document.createElement("img")
+      img.src = element.image.url
+      article.appendChild(img)
+      
+      let btn = document.createElement("button")
+      btn.innerText = "Borrar"
+      btn.addEventListener("click", ()=> {deleteMichis(element.id)}) // Estaba mandando el id de la foto, tenia que mandar el id del objeto
+      article.appendChild(btn)
+
+      favoritesMichis.appendChild(article)
+    });
   }
 }
 
-async function saveFavouriteMichis() {
+async function saveFavouriteMichis(michi) {
   const res = await fetch(API_URL_FAVOURITE, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       },
       body: JSON.stringify({ 
-          image_id: "12"
+          image_id: michi.id
         })
       })
   const data = await res.json()
 
   console.log("Save");
-  console.log(res);
+  console.log(data);
 
-  if (res.status !== "200")
+  if (res.status !== 200)
   {
-    spanError.innerHTML = "Hubo un error " + res.status;
+    SPAN_ERROR.innerHTML = "Hubo un error " + res.status;
   }
+  loadFavoritesMichis(API_URL_FAVOURITE)
+}
+
+async function deleteMichis(michi) {
+  console.log(API_URL_FAVOTITES_DELETE(michi));
+  const res = await fetch(API_URL_FAVOTITES_DELETE(michi),{
+    method: 'DELETE'})
+      
+  const data = await res.json()
+
+  console.log("DELETE");
+  console.log(data);
+
+  if (res.status !== 200)
+  {
+    SPAN_ERROR.innerHTML = "Hubo un error " + res.status;
+  }
+  loadFavoritesMichis(API_URL_FAVOURITE);
+}
+
+async function loadMichis() {
+  loadRandomMichis(API_URL_RANDOM);
+  loadFavoritesMichis(API_URL_FAVOURITE);
 }
 
 
 
-
-
-
-loadRandomMichis(API_URL_RANDOM);
-loadFavoritesMichis(API_URL_FAVOURITE);
+loadMichis()
