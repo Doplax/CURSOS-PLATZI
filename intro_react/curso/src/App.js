baa791b79d24ef7865674d9656e0c60c3ee7db16
Doplax <doplax@gmail.com>
2023-04-06 12:19:25 +0200

    feat: Ahora la barra de busqueda está conectada con los elementos

--- intro_react/curso/src/App.js
+++ intro_react/curso/src/App.js
@@ -1,37 +1,60 @@
 import React  from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import { TodoCounter } from './TodoCounter'
-import { TodoList } from './TodoList'
-import { TodoSearch } from './TodoSearch'
-import { TodoItem } from './TodoItem'
-import { CreateTodoButton } from './CreateTodoButton'
 
-const todos = [
-  { text: 'Cortar cebolla', completed: false},
+import './App.css';
+import { TodoCounter } from './components/TodoCounter/TodoCounter'
+import { TodoList } from './components/TodoList/TodoList'
+import { TodoSearch } from './components/TodoSearch/TodoSearch'
+import { TodoItem } from './components/TodoItem/TodoItem'
+import { CreateTodoButton } from './components/CreateTodoButton/CreateTodoButton'
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true},
   { text: 'Tomar curso', completed: false},
-  { text: 'Llorar con la llorona', completed: false},
+  { text: 'Llorar con la llorona', completed: true},
   { text: 'LALALALA', completed: false }
 ]
 
 
 function App(props) {
+  const [todos, setTodos] = React.useState(defaultTodos);
   const  [searchValue, setSearchValue] = React.useState('')
+
   
+  
+  const completedTodos = todos.filter((todo) => todo.completed == true).length;
+  const totalTodos = todos.length
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >=1) {
+      searchedTodos = defaultTodos
+    } else {
+      searchedTodos = todos.filter((todo) => {
+        const todoText = todo.text.toLowerCase()      
+        const searchText = searchValue.toLocaleLowerCase()
+        return todoText.includes(searchText)
+      })    
+     
+  }
+
   return (
     <React.Fragment>
-    <TodoCounter />
+    <TodoCounter
+      total={totalTodos}
+      completed = {completedTodos}
+    />
     <TodoSearch
       searchValue={searchValue}
       setSearchValue = {setSearchValue}
      />
     
     <TodoList> 
-      {todos.map(todo => (
+      {searchedTodos.map(todo => (
         <TodoItem 
           key={todo.text} 
           text={todo.text}
           completed={todo.completed}
+
         />
       ))}
    
