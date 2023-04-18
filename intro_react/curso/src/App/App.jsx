6ce948a1f58973afbe9d358496d5fb62bf8218d4
Doplax <doplax@gmail.com>
2023-04-18 16:01:12 +0200

    feat: Added custom hook useLocalStorage() to handle local storage logic for TODOs.

--- intro_react/curso/src/App/App.jsx
+++ intro_react/curso/src/App/App.jsx
@@ -9,25 +9,45 @@ import { AppUI } from "./AppUI";
 //];
 
 function useLocalStorage(itemName, initialValue) {
-    const localStorageItem  = localStorage.getItem(itemName)
-    let parsedItem;
+    const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(true);
+    const [item, setItem] = React.useState(initialValue)
 
-    if (!localStorage) {
-        localStorage.setItem(itemName, JSON.stringify(initialValue))
-        parsedItem = initialValue
-    } else {
-        parsedItem = JSON.parse(localStorageItem );
-    }
 
-    const [item, setItem] = React.useState(parsedItem);
+    React.useEffect(() => {
+        setTimeout(() => {
+            try {
+                const localStorageItem  = localStorage.getItem(itemName)
+                let parsedItem;
+            
+                if (!localStorage) {
+                    localStorage.setItem(itemName, JSON.stringify(initialValue))
+                    parsedItem = initialValue
+                } else {
+                    parsedItem = JSON.parse(localStorageItem );
+                }
+                setItem(parsedItem)
+            } catch(error) {
+                setError(error)
+            } finally {
+                setLoading(false);
+            }        
+        },1000)
+    })
+
+    //const [item, setItem] = React.useState(parsedItem);
 
     const saveItem = (newItem) => {
-        const stringifiedItem = JSON.stringify(newItem);
-        localStorage.setItem(itemName,stringifiedItem)
-        setItem(newItem);
+        try {
+            const stringifiedItem = JSON.stringify(newItem);
+            localStorage.setItem(itemName,stringifiedItem)
+            setItem(newItem);
+        } catch(error) {
+            setError(error)
+        }
     }
 
-    return [ item , saveItem ]
+    return { item, saveItem, loading, error }
 }
 
 
