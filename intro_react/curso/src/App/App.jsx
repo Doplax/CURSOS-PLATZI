b6b282c835589fbd484a3f789d431ce88fc27a85
Doplax <doplax@gmail.com>
2023-05-02 20:35:17 +0200

    Jugando con la composición de componentes

--- intro_react/curso/src/App/App.jsx
+++ intro_react/curso/src/App/App.jsx
@@ -1,16 +1,68 @@
-import React from "react";
-import { TodoProvider } from '../TodoContext/TodoContext';
-import { AppUI } from "./AppUI";
+import React, { Children } from "react";
+//import { TodoProvider } from '../TodoContext/TodoContext';
+//import { AppUI } from "./AppUI";
+//import { TodoCounter } from "../TodoCounter/TodoCounter";
+//import { TodoSearch } from "../TodoSearch/TodoSearch";
 
 
 
 function App() {
+    const [state, setState] = React.useState('hola') 
+    return(
+            <React.Fragment>
+                <TodoHeader>
+                    <TodoCounter/>
+                    <TodoSearch/>                </TodoHeader>
+                <TodoHeader/>
+                <TodoList>
+                    <TodoItem state={state} />
+                </TodoList>
+            </React.Fragment>
 
-    return (
-        <TodoProvider>
-            <AppUI/>
-        </TodoProvider>
     );
 }
+
+
+function TodoHeader({children}){
+    return(
+        <header>
+            {children}
+        </header>
+        
+    )
+}
+
+function TodoCounter(){
+    return(<p>TodoCounter</p>)
+}
+
+
+function TodoSearch(){
+    return(<p>TodoSearch</p>)
+}
+
+
+function TodoList({children}){
+    return(
+        <section className="todoList-container">            
+                {children}
+        </section>
+    )
+}
+
+function TodoItem( {state} ){
+    return (<p>{state}</p>)
+}
+
+
+
+//function App() {
+
+//    return (
+//        <TodoProvider>
+//            <AppUI/>
+//        </TodoProvider>
+//    );
+//}
 
 export { App };
